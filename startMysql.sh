4acb2dd183eef62bb83561c74737fd1f7ffffe59
Daniel Sousa <danielosousa@gmail.com>
2014-12-18 10:38:52 +0000

    bash script to start Mysql - MAMP

---
+++ startMysql.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+sh -c '/Applications/MAMP/Library/bin/mysqld_safe --defaults-file=/Applications/MAMP/tmp/mysql/my.cnf --port=3306 \
+--socket=/Applications/MAMP/tmp/mysql/mysql.sock --pid-file=/Applications/MAMP/tmp/mysql/mysql.pid \ 
+--log-error=/Applications/MAMP/logs/mysql_error_log.err --tmpdir=/Applications/MAMP/tmp/mysql/tmpdir \ 
+--datadir=/Library/Application\ Support/appsolute/MAMP\ PRO/db/mysql &'
